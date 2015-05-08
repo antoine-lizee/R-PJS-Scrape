5ce089e6bd56629524705f3fc3bc36accda9ee91
antoine.lizee <antoine.lizee@ucsf.edu>
2015-05-07 20:15:28 -0700

    first commit

---
+++ dumpHTML.js
@@ -0,0 +1,42 @@
+var page = require('webpage').create(),
+system = require('system'),
+fs = require('fs'),
+t, address,fileName;
+
+if (system.args.length === 1) {
+  console.log('Usage: dumpHTML.js <some URL>');
+  phantom.exit();
+}
+
+page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:36.0) Gecko/20100101 Firefox/36.0';
+page.customHeaders = { 'Referer':  'http://www.kayak.com/flights'};
+
+t = Date.now();
+address = system.args[1];
+fileName = address.split("/")[2] + "." + (new Date()).toISOString().slice(0,19) + '.html';
+fileName = 'test.html';
+
+page.open(address, function(status) {
+
+  if (status !== 'success') {
+    console.log('FAIL to load the address');
+  } else {
+    t = Date.now() - t;
+    console.log('Loading ' + system.args[1]);
+    console.log('Loading time ' + t + ' msec');
+
+    window.setTimeout(function () {
+      console.log('dumping HTML into ' + fileName);
+      try {
+        var f = fs.open(fileName, "w");
+        f.write(page.content);
+        f.close();
+      } catch (e) {
+        console.log(e);
+      }
+      console.log("rendering...");
+      page.render('test.jpeg', {format: 'jpeg', quality: '100'});
+      phantom.exit();
+        }, 40000); // Change timeout as required to allow sufficient time
+  }
+});
---
+++ getAndParse.R
@@ -0,0 +1,125 @@
+# getAndParse.R
+# Main script to query the kayak page of interest, parse the results, and feed the database.
+#
+# Copyright Antoine Lizee 2015/05 antoine.lizee@gmail.com 
+
+# Parameters --------------------------------------------------------------
+
+doStore <- T
+doQuery <- T
+
+# File names
+pageFile <- "test.html"
+dbname <- "kayakResults.sqlite"
+
+# URLs
+baseURL <- "http://www.kayak.com/flights"
+destinations <- "SFO-PAR"
+goDates <-"2015-05-29-flexible"
+returnDates <- "2015-06-24-flexible"
+
+# Build URL
+fullUrl <- paste(baseURL, destinations, goDates, returnDates, sep = "/")
+
+
+# Initialization ----------------------------------------------------------
+suppressMessages({
+  library("XML")
+  library("RSQLite")
+})
+
+cat("# KAYAK scraping script run as", system("whoami", intern = T), "on", format(t0 <- Sys.time()) ,"...\n")
+
+# Get in the correct directory when launched from 'Rscript'
+cmdArgs <- commandArgs(trailingOnly = FALSE)
+needle <- "--file="
+match <- grep(needle, cmdArgs)
+if (length(match) > 0) {
+  setwd(dirname(normalizePath(sub(needle, "", cmdArgs[match]))))
+}
+
+# Launch the scraping -----------------------------------------------------
+
+if (doQuery) {
+  
+  cat("## Querying webpage...")
+  ti <- Sys.time()
+  system(paste0('~/local_software/phantomjs/bin/phantomjs dumpHTML.js ', fullUrl))
+  cat("done. (in ", format(Sys.time() - ti),")\n", sep = "")
+  
+}
+
+# Parse the page ----------------------------------------------------------
+
+cat("## Parsing webpage...")
+ti <- Sys.time()
+# Get the price matrix
+flexTable <- readHTMLTable(pageFile, stringsAsFactors = F)$flexmatrixcontent
+flexResults <- sapply(flexTable[2:8, 2:8], function(v) as.numeric(gsub("$", "", v, fixed = T)))
+dimnames(flexResults) <- list( flexTable[2:8,1], flexTable[1,2:8])
+
+# Get the first displayed results
+page <- htmlParse(pageFile)
+xPath1 <- '//div[@class="resultsWrapperSection flightlist"]/*'
+xPathPrice <- './/div[@class="pricerange "]'
+xPathCompany <- './/div[@class="airlineName "]'
+xPathCompany <- './/div[starts-with(@class, "airlineName")]'
+xPathDurations <- './/div[@class="duration"]'
+xPathDates <- './/div[@class="baggageFees hdr_message"]'
+results <- xpathApply(page, xPath1, function(n) {
+  priceNode <- getNodeSet(n, xPathPrice)
+  if(length(priceNode) != 1) {
+    return(NULL)
+  }
+  price <- as.numeric(gsub("\n|\\$", "", xmlValue(priceNode[[1]])))
+  
+  company <- gsub("\n", "", xmlValue(getNodeSet(n, xPathCompany)[[1]]))
+  
+  durationInfo <- xpathSApply(n, xPathDurations, function(cn) gsub("\n|\\$", "", xmlValue(cn)))
+  durations <- as.numeric(as.difftime(durationInfo, format = "%Hh %Mm "))
+  
+  dateInfo <-  gsub("\n", "", xmlValue(getNodeSet(n, xPathDates)[[1]]))
+  tripDuration <- as.numeric(gsub(".*Trip spans |days.*$", "", dateInfo))
+  dates <- strsplit(dateInfo, split = " – ")[[1]]
+  goDate <- dates[1]
+  returnDate <- strsplit(dates[2], " [[:punct:]] ")[[1]][1]
+  
+  return(data.frame(price = price, company = company, 
+                    d1 = durations[1], d2 = durations[2], 
+                    trip_duration = tripDuration,
+                    go_date = goDate, return_date = returnDate))
+})
+
+results <- do.call(rbind,results)
+cat("done. (in ", format(Sys.time() - ti),")\n", sep = "")
+
+
+# Store Results -------------------------------------------------------------------
+
+if (doStore) {
+  
+  cat("## Storing results...")
+  
+  addTimeStamp <- function(df) {
+    data.frame(df, created_at = as.character(Sys.time()))
+  }
+  
+  flexResultsTable <- melt(flexResults, value.name = "price", varnames = c("go_date", "return_date"))
+  finalTables <- lapply(list(results, flexResultsTable), addTimeStamp)
+  names(finalTables) <- c("results", "flexResults")#, "companyResults")
+  
+  tryCatch({
+    con <- dbConnect(SQLite(), dbname = dbname)
+    isWritten <- mapply(function(name, table) dbWriteTable(con, name, table, append = T), names(finalTables), finalTables)
+    if (!all(isWritten)) {
+      cat("## ERROR IN WRITING DATA ##")
+      print(isWritten)
+    }
+  },
+  finally = dbDisconnect(con))
+  cat("done. (in ", format(Sys.time() - ti),")\n", sep = "")
+  
+}
+
+cat("## Completed script in ", format(Sys.time() - t0),", exiting now. Thank you!\n", sep = "")
+
